d53cca7df59a3b2b0d4bb33affbd15156bd8ff43
Wellington Soares <wellington.soares@totvs.com.br>
2023-10-05 16:01:19 -0300

    Add test Certmanager

---
+++ executors/ginkgo/certmanager/README.md
@@ -0,0 +1,41 @@
+# Name: Certmanager
+
+## TestKube Type: Golang/Ginkgo
+
+## Verifications:
+
+- Verify login attempt using access credentials and get session token. ( Expected: "200" )
+
+- Check if the configured repositories have a successful connection. ( Expected: "Connection: Successful" )
+ 
+## URL:
+
+- Service: http://argo-cd-argocd-server.tks-system.svc.cluster.local
+
+## Endpoints:
+
+- /api/v1/session (requires authentication)
+- /api/v1/repositories (requires authentication)
+
+## Variables:
+
+- USER string
+- PASS string
+
+## Create Test:
+
+```
+kubectl testkube create test --name check-argocd --type postman/collection --test-content-type git-file --git-uri https://github.com/cloud104/automated-tests.git --git-branch master --git-path executors/postman/argocd/argocd.json
+```
+
+### Or
+
+```
+kubectl create -f https://raw.githubusercontent.com/cloud104/automated-tests/master/executors/postman/argocd/argocd.yaml
+```
+
+## Run Test:
+
+```
+kubectl testkube run test check-argocd --secret-variable USER=$USERNAME --secret-variable PASS=$PASSWORD
+```
---
+++ executors/ginkgo/certmanager/certmanager_test.go
@@ -0,0 +1,81 @@
+package certmanager_test
+
+import (
+	"context"
+	"os"
+	"path"
+
+	"github.com/totvs-cloud/go-manifest"
+
+	"github.com/hashicorp/go-multierror"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+var manifests *manifest.Reader
+
+var _ = BeforeSuite(func() {
+	config, err := loadRestConfig()
+	Expect(err).NotTo(HaveOccurred())
+
+	mr, err := manifest.NewReader("totvs-cloud", config)
+	Expect(err).NotTo(HaveOccurred())
+
+	manifests = mr
+})
+
+var _ = AfterSuite(func() {
+	m, err := manifests.FromPath("./manifest.yaml", false)
+	Expect(err).NotTo(HaveOccurred())
+
+	err = m.Delete(context.Background())
+	Expect(err).NotTo(HaveOccurred())
+})
+
+var _ = Describe("Certmanager Test", func() {
+	It("should return success apply", func() {
+		config, err := loadRestConfig()
+		Expect(err).NotTo(HaveOccurred())
+
+		// Instantiate a new ManifestReader by specifying the field manager and the Kubernetes cluster configuration
+		mr, err := manifest.NewReader("totvs-cloud", config)
+		Expect(err).NotTo(HaveOccurred())
+
+		// Create a new Manifest object
+		m, err := mr.FromPath("./manifest.yaml", false)
+		Expect(err).NotTo(HaveOccurred())
+
+		// Apply the manifest using Server-Side Apply
+		err = m.Apply(context.Background())
+		Expect(err).NotTo(HaveOccurred())
+
+		GinkgoWriter.Println("Manifest applied successfully!")
+	})
+})
+
+func loadRestConfig() (*rest.Config, error) {
+	var result error
+
+	// Attempt to retrieve the Kubernetes cluster configuration first from the in-cluster environment
+	config, err1 := rest.InClusterConfig()
+	if err1 == nil {
+		return config, nil
+	}
+
+	result = multierror.Append(result, err1)
+
+	// If that doesn't work, try getting it locally using the kubeconfig file in your home directory
+	homeDir, err2 := os.UserHomeDir()
+	if err2 != nil {
+		return nil, multierror.Append(result, err2)
+	}
+
+	config, err3 := clientcmd.BuildConfigFromFlags("", path.Join(homeDir, "/.kube/config"))
+	if err3 != nil {
+		return nil, multierror.Append(result, err3)
+	}
+
+	return config, nil
+}
