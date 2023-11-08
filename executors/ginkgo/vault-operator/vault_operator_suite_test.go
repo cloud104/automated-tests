54a275ed2dc903d3cf28d26c62c13ef253dbce15
Tiago Angelo <tiago.angelo@totvs.com.br>
2023-11-08 15:49:53 -0300

    Install and test Vault using the Operator

--- executors/ginkgo/vault-operator/vault_operator_suite_test.go
+++ executors/ginkgo/vault-operator/vault_operator_suite_test.go
@@ -1,13 +1,80 @@
 package vault_operator_test
 
 import (
+	"context"
+	"log/slog"
+	"net/http"
+	"os"
 	"testing"
+	"time"
 
+	"github.com/hashicorp/vault-client-go"
+	"github.com/lmittmann/tint"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+
+	"github.com/cloud104/automated-tests/executors/ginkgo/vault-operator/internal/k8s"
+	"github.com/cloud104/automated-tests/executors/ginkgo/vault-operator/internal/temporary"
+	intlvault "github.com/cloud104/automated-tests/executors/ginkgo/vault-operator/internal/vault"
+	"github.com/cloud104/automated-tests/executors/ginkgo/vault-operator/internal/vaultoperator"
 )
 
 func TestVaultOperator(t *testing.T) {
+	logger := slog.New(tint.NewHandler(os.Stderr, &tint.Options{
+		AddSource:  true,
+		Level:      slog.LevelDebug,
+		TimeFormat: time.Kitchen,
+	}))
+	slog.SetDefault(logger)
+
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "VaultOperator Suite")
+}
+
+type Testing struct {
+	TemporaryNamespace *temporary.Namespace
+	VaultOperator      *vaultoperator.Client
+	VaultSecrets       vault.Secrets
+}
+
+func initialize(contextContext context.Context) (*Testing, error) {
+	config, err := k8s.NewConfig()
+	if err != nil {
+		return nil, err
+	}
+
+	clientset, err := k8s.NewClientset(config)
+	if err != nil {
+		return nil, err
+	}
+
+	coreClient := k8s.NewCoreV1Client(clientset)
+	temporaryNamespace := temporary.NewNamespace(coreClient)
+
+	manifestReader, err := k8s.NewManifestReader(config)
+	if err != nil {
+		return nil, err
+	}
+
+	vaultOperatorClient := vaultoperator.NewClient(coreClient, manifestReader)
+
+	vaultConfig, err := intlvault.NewConfig()
+	if err != nil {
+		return nil, err
+	}
+
+	httpClient := &http.Client{}
+
+	vaultClient, err := intlvault.NewClient(contextContext, vaultConfig, httpClient)
+	if err != nil {
+		return nil, err
+	}
+
+	vaultSecrets := intlvault.NewSecrets(vaultClient)
+
+	return &Testing{
+		TemporaryNamespace: temporaryNamespace,
+		VaultOperator:      vaultOperatorClient,
+		VaultSecrets:       vaultSecrets,
+	}, nil
 }
--- executors/ginkgo/vault-operator/vault_test.go
+++ executors/ginkgo/vault-operator/vault_test.go
@@ -1,12 +1,74 @@
 package vault_operator_test
 
 import (
+	_ "embed"
+	"time"
+
+	"github.com/hashicorp/vault-client-go"
+	"github.com/hashicorp/vault-client-go/schema"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-
-	"UNKNOWN_PACKAGE_PATH"
+	"github.com/totvs-cloud/go-manifest"
 )
 
-var _ = Describe("Vault", func() {
+var _ = Describe("Installing Vault using the Operator", Ordered, func() {
+	var (
+		testing   *Testing
+		namespace string
+		manifests manifest.List
+	)
 
+	BeforeAll(func(ctx SpecContext) {
+		t, err := initialize(ctx)
+		Expect(err).NotTo(HaveOccurred())
+
+		ns, err := t.TemporaryNamespace.Create(ctx, "vault-operator-test")
+		Expect(err).NotTo(HaveOccurred())
+
+		testing = t
+		namespace = ns
+	})
+
+	AfterAll(func(ctx SpecContext) {
+		err := testing.VaultOperator.CleanUp(ctx, namespace, manifests)
+		Expect(err).NotTo(HaveOccurred())
+
+		err = testing.TemporaryNamespace.CleanUp(ctx, namespace)
+		Expect(err).NotTo(HaveOccurred())
+	})
+
+	When("the Operator CRD is applied", func() {
+		BeforeAll(func(ctx SpecContext) {
+			m, err := testing.VaultOperator.ApplyManifests(ctx, namespace)
+			Expect(err).NotTo(HaveOccurred())
+
+			manifests = m
+		})
+
+		It("should eventually have 1 ready pod", func(ctx SpecContext) {
+			countReadyPods := func() int {
+				count, err := testing.VaultOperator.CountReadyPods(ctx, namespace)
+				Expect(err).NotTo(HaveOccurred())
+
+				return count
+			}
+			Eventually(countReadyPods, ctx).Should(Equal(1))
+		}, SpecTimeout(time.Second*30))
+	})
+
+	When("the installation process is complete", func() {
+		It("should be able to add a new secret to Vault", func(ctx SpecContext) {
+			req := schema.KvV2WriteRequest{Data: map[string]any{"password": "baboseiras"}}
+			_, err := testing.VaultSecrets.KvV2Write(ctx, "robopato", req, vault.WithMountPath("secret"))
+			Expect(err).NotTo(HaveOccurred())
+		})
+
+		It("should be able to read the new secret from Vault", func(ctx SpecContext) {
+			s, err := testing.VaultSecrets.KvV2Read(ctx, "robopato", vault.WithMountPath("secret"))
+			Expect(err).NotTo(HaveOccurred())
+
+			password := s.Data.Data["password"]
+			Expect(password).To(Equal("baboseiras"))
+		})
+	})
 })
