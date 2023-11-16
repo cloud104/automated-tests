e5a3956826794a3e6c760a2fab3324886d23891f
Tiago Angelo <tiago.angelo@totvs.com.br>
2023-11-16 18:34:24 -0300

    Add k8s client and core v1 client creation functions

--- executors/ginkgo/crossplane/internal/wire/providers.go
+++ executors/ginkgo/crossplane/internal/wire/providers.go
@@ -2,12 +2,22 @@ package wire
 
 import (
 	"github.com/google/wire"
+	corev1 "k8s.io/api/core/v1"
+
+	"github.com/cloud104/automated-tests/executors/ginkgo/crossplane/internal/config"
+	"github.com/cloud104/automated-tests/executors/ginkgo/crossplane/internal/k8s"
 )
 
 //nolint:unused // This function is used during compile-time to generate code for dependency injection
 var providers = wire.NewSet(
+	config.NewProfile,
+	k8s.NewClientset,
+	k8s.NewConfig,
+	k8s.NewCoreV1Client,
+	k8s.NewNamespace,
 	wire.Struct(new(Test), "*"),
 )
 
 type Test struct {
+	Namespace *corev1.Namespace
 }
