1386557c5984fc3091317229de0d85cc433609c9
Tiago Angelo <tiago.angelo@totvs.com.br>
2023-12-01 09:55:35 -0300

    Add configuration files for test and profile settings

---
+++ executors/ginkgo/ingress-monitor-controller/internal/config/profile.go
@@ -0,0 +1,20 @@
+package config
+
+import (
+	"fmt"
+
+	env "github.com/caarlos0/env/v10"
+)
+
+type Profile struct {
+	Active string `env:"ACTIVE" envDefault:"kubernetes"`
+}
+
+func NewProfile() (*Profile, error) {
+	cfg := &Profile{}
+	if err := env.ParseWithOptions(cfg, env.Options{Prefix: "PROFILE_"}); err != nil {
+		return nil, fmt.Errorf("error parsing Vault environment variables: %w", err)
+	}
+
+	return cfg, nil
+}
